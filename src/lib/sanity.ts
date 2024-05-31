7b8088120d07132cc4b088e135a2cdb546ec752c
sandeep vemula <sandeepvemula408@gmail.com>
2024-05-31 13:39:33 +0530

    changes

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
+  projectId: "37z689uz",
+  dataset: "production",
   apiVersion: "2022-03-25",
   useCdn: true,
 });
